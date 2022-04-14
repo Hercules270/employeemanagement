2ba6bda531ac6513109a8024cd19fe09d08f9ef9
Irakli Bezhuashvili <ibezh17@freeuni.edu.ge>
2022-04-14 23:19:23 +0200

    Added swagger ui

--- src/main/java/com/manage/employeemanagement/config/SecurityConfiguration.java
+++ src/main/java/com/manage/employeemanagement/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -51,5 +52,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return NimbusJwtDecoder.withJwkSetUri(this.jwkUri).build();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/swagger-ui/**");
+    }
 }
 
